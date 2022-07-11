767df0930d55aebaf19903a63a6a5448772b3a4f
Franco Cardenas <gustavo260123@gmail.com>
2022-07-10 20:15:15 -0700

    Adding the print_percent.c function

---
+++ print_percent.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_percent - Prints a percent symbol
+ * @list: arg_list || list of arguments
+ * Return: Return how many characters are printed.
+ */
+int print_percent(__attribute__((unused))va_list list)
+{
+	_putchar('%');
+	return (1);
+}
