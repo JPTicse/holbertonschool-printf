b0177f94facd5151caf66fefe15c2d29961f9fad
JPTicse <jpticse@gmail.com>
2022-07-10 22:02:37 -0500

    Adding condition match and get function in reader.c

--- reader.c
+++ reader.c
@@ -17,7 +17,29 @@ int reader(const char *format, get_function f_list[], va_list arg_list)
 	{
 		if (format[i] == '%')
 		{
-			/*add the struct get_functions conditions*/
+			for (j = 0; f_list[j].letter != NULL; j++)
+			{
+				if (format[i + 1] == f_list[j].letter[0])
+				{
+					check_print = f_list[j].f(arg_list);
+					if (check_print == -1)
+						return (-1);
+					char_counter += check_print;
+					break;
+				}
+			}
+			if (f_list[j].letter == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					char_counter = char_counter + 2;
+				}
+				else
+					return (-1);
+			}
+			i = i + 1;
 		}
 		else
 		{
