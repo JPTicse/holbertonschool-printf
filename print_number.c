b756101fd92eb157587c36aa7f2fa11e9acdc924
JPTicse <jpticse@gmail.com>
2022-07-10 22:21:41 -0500

    Adding algorithm to print each character of the number in print_number.c

--- print_number.c
+++ print_number.c
@@ -19,5 +19,15 @@ int print_number(va_list args)
 		len += _putchar('-');
 		num = n * -1;
 	}
+	else
+		num = n;
+	for (; num / div > 9; )
+		div *= 10;
+	for (; div != 0; )
+	{
+		len += _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
 	return (len);
 }
