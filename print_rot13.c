66c60d1a59a551ddeaecec756848301caa3e48f3
JPTicse <jpticse@gmail.com>
2022-07-11 22:59:04 -0500

    Adding  rot_13 function

---
+++ print_rot13.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rot13 - Converts string to rot13
+ * @list: string to convert
+ * Return: converted string
+ */
+int print_rot13(va_list list)
+{
+	int i;
+	int x;
+	char *str;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(list, char *);
+	/*Add condition*/
+	return (i);
+}
